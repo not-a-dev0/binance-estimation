4204c9cebf1e5c8891c7d823d1a0a54f67e6b1a8
AButorov <40419739+AButorov@users.noreply.github.com>
2019-01-28 21:17:19 +0700

    3

--- prog/main.py
+++ prog/main.py
@@ -1,11 +1,21 @@
+from datetime import datetime
+
+
 from binance_api import Binance
 bot = Binance(
     API_KEY='D7...Ejj',
     API_SECRET='gwQ...u3A'
 )
 
-print('klines', bot.klines(
+slKLines = bot.klines(
     symbol='BNBBTC',
     interval='1d',
-    limit=4
-))
+    limit=20)
+
+
+# 
+print('BNB/BTC 1d 4num')
+i=0
+for slKLine in slKLines:
+    i+=1
+    print(i,'Дата:',str(datetime.fromtimestamp(int(slKLine[0]))),'Покупка:',slKLine[3],'Продажа',slKLine[2],'Обьем',slKLine[7],'Сделок',slKLine[8])
