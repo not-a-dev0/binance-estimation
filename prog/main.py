42281713e561bb9c7ac2abfee47858f7dc810e1e
AButorov <40419739+AButorov@users.noreply.github.com>
2019-01-28 20:28:53 +0700

    2

--- prog/main.py
+++ prog/main.py
@@ -1,1 +1,11 @@
-print ('test git push -u origin master')
+from binance_api import Binance
+bot = Binance(
+    API_KEY='D7...Ejj',
+    API_SECRET='gwQ...u3A'
+)
+
+print('klines', bot.klines(
+    symbol='BNBBTC',
+    interval='1d',
+    limit=4
+))
