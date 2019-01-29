5135c16c44dff21c0c710c89d939171be7a507eb
AButorov <40419739+AButorov@users.noreply.github.com>
2019-01-29 20:30:43 +0700

    5

--- prog/main.py
+++ prog/main.py
@@ -10,15 +10,19 @@ bot = Binance(
 slKLines = bot.klines(
     symbol='BNBBTC',
     interval='1d',
-    limit=20)
+    limit=10)
 
 
 # 
-print('BNB/BTC 1d 4num')
+print('BNB/BTC 1d 10num')
 i=0
+# минимальная цена за весь период для покупки
 fPriceMin = 99999999999999999.0
+# максимальная цена за весь период для продажи
 fPriceMax = 0.0
+# уровни покупок
 spBuy  = []
+# уровни продаж
 spSell = []
 
 for slKLine in slKLines:
@@ -39,3 +43,21 @@ print ('min=', fPriceMin, 'max=', fPriceMax)
 print(spBuy)
 print(spSell)
 
+# флаг текущего состояния
+# готовность покупать - False
+# готовность продовать - True
+bFlOperation = False
+# количество первой валюты, просто для анализа прибыли
+fCurrency = 100.00
+# прибыль
+fProfit = 0.00
+# количество сделок
+iDeal = 0
+
+
+
+for slKLine in slKLines:
+    if bFlOperation :
+        pass
+    else:
+        pass
